3c4fa31478738b6dbc581ff8e5fb764f92a29c63
Deepak <ds4754369@gmail.com>
2025-01-25 12:36:43 +0530

    fix image error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from "react";
-import Image from "next/image";
+//import Image from "next/image";
 
 // Internal dependencies
 import { getContent } from "@/utils/getContent";
@@ -20,11 +20,9 @@ export default async function _404(): Promise<ReactNode> {
     <div className="size-full bg-black-medium">
       <Header navLinks={navLinks} />
       <div className="mx-auto flex min-h-screen max-w-view flex-col items-center justify-center gap-2 bg-gpt-darker p-8 text-white">
-
         <h2 className="font-mono text-heading text-gray-400">
           The Computer Science Department Hansraj College
         </h2>
-
       </div>
       {content && <Footer content={content} />}
     </div>
